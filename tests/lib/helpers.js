f45ab2995f203bc408e0851b7da8af8ac7f9fd31
Fischer.json <foxbrush@Fischerjsons-MacBook-Pro.local>
2016-01-07 18:51:31 +0800

    Inital create

--- tests/lib/helpers.js
+++
@@ -1,8 +0,0 @@
-/*
-    This file contains local helpers for casper testing. The `helpers` global
-    included in all UI test files is the result of merging this file onto the
-    global helpers which are in marketplace-gulp/tests/casper-helpers.js.
-*/
-
-module.exports = {
-};
--- tests/serve.sh
+++
@@ -1,3 +0,0 @@
-make build
-MKT_COMPILED=1 make serve &
-sleep 10
